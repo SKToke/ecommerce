4f12b7f3ccc2f3f3f582fbf0efd6ac6d8f2afb2b
solaimanH <tokekhan@gmail.com>
2021-11-02 22:09:31 +0600

    redme updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /* create admin */
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'is_admin' => true,
+            'password' => bcrypt('admin'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        /* other seeders */
         Product::factory(10)->create();
         Order::factory(10)->create();
     }
